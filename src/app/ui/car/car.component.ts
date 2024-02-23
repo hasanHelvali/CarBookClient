1215742eedd60ffa407aeb345faecd0d4955d3a7
HasanHelvali <hasan.helvali.git@gmail.com>
2024-02-24 00:08:21 +0300

    CarPricings

--- src/app/ui/car/car.component.ts
+++ src/app/ui/car/car.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CustomHttpClient } from '../services/customHttpClient.service';
-import { CarWithBrand } from '../models/car-with-brand';
+import { CarsWithPricing } from '../models/car-with-pricing';
 
 @Component({
   selector: 'app-car',
@@ -12,14 +12,14 @@ export class CarComponent {
    *
    */
 
-  allCarData:CarWithBrand[];
+  allCarData:CarsWithPricing[];
   constructor(private httpClientService:CustomHttpClient) {
-    this.getCar();
+    this.getCarPricingWithCar();
   }
 
 
-  getCar(){
-    this.httpClientService.get<CarWithBrand>({controller:"cars/getcarwithbrand"}).subscribe({
+  getCarPricingWithCar(){
+    this.httpClientService.get<CarsWithPricing>({controller:"carpricings"}).subscribe({
       next:(data)=>{
         this.allCarData=data;
       },
@@ -27,41 +27,41 @@ export class CarComponent {
       }
     })
   }
-  getCarById(CarId:string){
-    this.httpClientService.get<CarWithBrand>({controller:"cars/getcarwithbrand"},CarId).subscribe({
-      next:(data)=>{
-        console.log(data);
-        this.allCarData=data;
-      },
-      error:err=>{
-      }
-    })
-  }
+  // getCarById(CarId:string){
+  //   this.httpClientService.get<CarsWithPricing>({controller:"carpricings"},CarId).subscribe({
+  //     next:(data)=>{
+  //       console.log(data);
+  //       this.allCarData=data;
+  //     },
+  //     error:err=>{
+  //     }
+  //   })
+  // }
   
-  createCar(Car:CarWithBrand){
-    this.httpClientService.post({controller:"cars/getcarwithbrand"},Car).subscribe({
-      next:data=>{
-      },
-      error:err=>{
-      }
-    })
-  }
+  // createCar(Car:CarsWithPricing){
+  //   this.httpClientService.post({controller:"carpricings"},Car).subscribe({
+  //     next:data=>{
+  //     },
+  //     error:err=>{
+  //     }
+  //   })
+  // }
   
-  updateCar(Car:CarWithBrand){
-    this.httpClientService.post({controller:"cars/getcarwithbrand"},Car).subscribe({
-      next:data=>{
-      },
-      error:err=>{
-      } 
-    })
-  }
+  // updateCar(Car:CarsWithPricing){
+  //   this.httpClientService.post({controller:"carpricings"},Car).subscribe({
+  //     next:data=>{
+  //     },
+  //     error:err=>{
+  //     } 
+  //   })
+  // }
   
-  deleteCar(CarId:string){
-    this.httpClientService.delete({controller:"cars",action:"getcarwithbrand"},CarId).subscribe({
-      next:data=>{
-      },
-      error:err=>{
-      }
-    })
-  }
+  // deleteCar(CarId:string){
+  //   this.httpClientService.delete({controller:"cars",action:"getcarwithbrand"},CarId).subscribe({
+  //     next:data=>{
+  //     },
+  //     error:err=>{
+  //     }
+  //   })
+  // }
 }
---
+++ src/app/ui/models/car-with-pricing.ts
@@ -0,0 +1,7 @@
+export class CarsWithPricing{
+      carPricingID:number; 
+      brand:string
+      model:string;
+      amount:string;
+      coverImageUrl:string; 
+}
